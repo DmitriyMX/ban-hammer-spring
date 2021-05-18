4aa0954ae31b9404dd673d62828b11bee6a510a1
DmitriyMX <dimon550@gmail.com>
2021-05-18 16:47:09 +0300

    base

---
+++ src/main/java/ru/dmitriymx/plugin/BanHammerPlugin.java
@@ -0,0 +1,12 @@
+package ru.dmitriymx.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class BanHammerPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		this.getLogger().info("Hello!");
+	}
+}
